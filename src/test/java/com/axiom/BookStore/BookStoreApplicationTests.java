083fb7b67853b047230bd02ce64565f81ab3e3ad
sunil.a.george <sunil.a.george@accenture.com>
2020-02-28 12:41:42 -0500

    initial commit

---
+++ src/test/java/com/axiom/BookStore/BookStoreApplicationTests.java
@@ -0,0 +1,14 @@
+package com.axiom.BookStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
